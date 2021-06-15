db9aa23a7f52e7a5e1673c4d9856fe0fa60553d7
Jialiang Chen <cjl2559@163.com>
2021-06-15 15:47:54 +0800

    add IR: function invoke

--- test/a.c
+++ test/a.c
@@ -1,6 +1,17 @@
+int f(int a, int b) {
+    return 3;
+}
+int fadd(int a1, int a2) {
+    return a1+a2;
+}
+
 int main(){
     int a = 1;
     int b = 2;
     int c = 4;
 
+    c = fadd(a, b);
+    c = f(a,b);
+
+    return c;
 }
