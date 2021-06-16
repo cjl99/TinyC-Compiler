b97567ac3243777a134419e10f571f44aff3ff90
ZXXYy <308169188@qq.com>
2021-06-16 15:07:24 +0800

    add [] operator, e.g. str[2]

--- test/a.c
+++ test/a.c
@@ -1,26 +1,7 @@
 extern int printf(char* format);
-
 int main(){
-
-    int s = 0;
-    int i;
-    int j;
-
-    if(i==0) {
-        printf("%d", ++s);
-    }
-    else {
-        s = s+10;
-        printf("%d", s);
-    }
-
-    for(i=0; i<=5; ++i) {
-        for(j=0; j<=5; ++j) {
-            printf("%d %d\n",i, j);
-            if(j==2) break;
-        }
-
-    }
+    char *str ="abc";
+    printf("%c\n", str[1+1]);
 
     return 0;
 }
